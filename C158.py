2164a612220c631ecf5ab0c2ef5816cdcb5d1430
arjunTheCoder <82092827+arjun380@users.noreply.github.com>
2022-02-22 20:01:18 +0530

    Add files via upload

---
+++ C158.py
@@ -0,0 +1,20 @@
+#data type error
+from tkinter import *
+from tkinter import messagebox
+root=Tk()
+root.title("Addition")
+root.geometry("600x400")
+input_box = Entry(root)
+input_box.pack
+
+def addition():
+    number = 5
+    get_input = input_box.get()
+    try:
+        print(number + get_input)
+    except(TypeError):
+        messagebox.showinfo("Error", "Cannot add two diifrent data types: integer and string")
+
+button = Button(root , text= "addition", command = addition)
+button.pack()
+root.mainloop()
